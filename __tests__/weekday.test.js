1fa63a351f257f862644fcd746d9dd1608fbb5d2
Ryan Ashby <rjapsu160@gmail.com>
2021-11-10 11:42:08 -0800

    add first test for weekday constructor. good fail - undefined.
    co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

---
+++ __tests__/weekday.test.js
@@ -0,0 +1,11 @@
+import Weekday from "../src/js/weekday.js";
+
+describe("Weekday", () => {
+
+  test("It should create a Weekday object with a day, month, and year", () => {
+    const weekday1 = new Weekday(12, 12, 1912);
+    expect(weekday1.day).toEqual(12);
+    expect(weekday1.month).toEqual(12);
+    expect(weekday1.year).toEqual(1912);
+  });
+});
---
+++ src/js/weekday.js
@@ -0,0 +1,5 @@
+export default function Weekday(day, month, year) {
+    // this.day = day;
+    // this.month = month;
+    // this.year = year;
+  };
