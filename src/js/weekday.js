5160772d0db8b7a957bfd4a6e41eec1f54fe65c7
Ryan Ashby <rjapsu160@gmail.com>
2021-11-10 11:43:33 -0800

    pass first test, constructing weekday object with day, month, year.
    co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- src/js/weekday.js
+++ src/js/weekday.js
@@ -1,5 +1,5 @@
 export default function Weekday(day, month, year) {
-    // this.day = day;
-    // this.month = month;
-    // this.year = year;
+    this.day = day;
+    this.month = month;
+    this.year = year;
   };
