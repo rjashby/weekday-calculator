ed0542de11436ef808013229643ff0848dadb90b
Ryan Ashby <rjapsu160@gmail.com>
2021-11-10 10:21:15 -0800

    install node modules and set up launch.json file.
    co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'JustPleaseWork',
+      title: 'Weekday Calculator',
       template: './src/index.html',
       inject: 'body'
     })
